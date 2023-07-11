78bc6336ab581a487ee6a10060bfecd1cd13593f
Frank Martin <f.martin@iknl.nl>
2023-07-11 17:00:02 +0200

    Added simple tests for the `execute_sql` and `query_sql` functions

--- database-connector/tests/main.py
+++ database-connector/tests/main.py
@@ -1,4 +1,4 @@
-from ohdsi.database_connector import Connect
+from ohdsi.database_connector import Connect, Sql
 
 connection_details = Connect.create_connection_details(
     "postgresql",
@@ -10,6 +10,14 @@ connection_details = Connect.create_connection_details(
 con = Connect.connect(connection_details)
 
 print(con)
+
+result = Sql.query_sql(con, "SELECT * FROM omopcdm.person LIMIT 3")
+print(result)
+
+Sql.execute_sql(
+    con,
+    "CREATE TABLE foo (bar INT);"
+)
 
 Connect.disconnect(con)
 
