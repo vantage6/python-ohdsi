a580f9120e55df93887082bee8fb89827304ae50
bartvanb <b.vanbeusekom@iknl.nl>
2025-07-31 17:04:39 +0200

    Update to python 3.13

--- database-connector/setup.py
+++ database-connector/setup.py
@@ -1,38 +1,36 @@
 import codecs
-
-from setuptools import setup, find_namespace_packages
 from pathlib import Path
+
+from setuptools import find_namespace_packages, setup
 
 # get current directory
 here = Path(__file__).parent.absolute()
 parent_dir = here.parent.absolute()
 
 # get the long description from the README file
-with codecs.open(parent_dir / 'README.md', encoding='utf-8') as f:
+with codecs.open(parent_dir / "README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 # get the version from the parent dir
-with open(parent_dir / 'VERSION') as f:
+with open(parent_dir / "VERSION") as f:
     version = f.read().strip()
 
 # setup the package
 setup(
-    name='ohdsi-database-connector',
+    name="ohdsi-database-connector",
     version=version,
-    description='Python wrapper for the OHDSI R packages',
+    description="Python wrapper for the OHDSI R packages",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/vantage6/python-ohdsi',
-    packages=find_namespace_packages(include=['ohdsi.*']),
-    python_requires='>=3.10',
+    long_description_content_type="text/markdown",
+    url="https://github.com/vantage6/python-ohdsi",
+    packages=find_namespace_packages(include=["ohdsi.*"]),
+    python_requires=">=3.13",
     install_requires=[
-        'rpy2==3.5.12',
+        "rpy2==3.5.12",
     ],
-    extras_require={
-        'dev': []
-    },
+    extras_require={"dev": []},
     package_data={
-        'ohdsi.database_connector.java': ['*.jar'],
+        "ohdsi.database_connector.java": ["*.jar"],
     },
     # entry_points={
     #     'console_scripts': [
