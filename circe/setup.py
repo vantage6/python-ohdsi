28b023cad8ab9c446ecb56a842ebe7c587fb435e
Frank <mail@franky.codes>
2025-10-23 16:27:07 +0200

    Added project toml to the `circe` package

--- circe/setup.py
+++ circe/setup.py
@@ -4,22 +4,20 @@ from pathlib import Path
 from setuptools import find_namespace_packages, setup
 
 # get current directory
-here = Path(__file__).parent.absolute()
-parent_dir = here.parent.absolute()
-
+here = Path(__file__).absolute()
 # get the long description from the README file
-with codecs.open(parent_dir / "README.md", encoding="utf-8") as f:
+with codecs.open(here / "README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 # get the version from the parent dir
-with open(parent_dir / "VERSION") as f:
-    version = f.read().strip()
+# with open(here / "VERSION") as f:
+#     version = f.read().strip()
 
 # setup the package
 setup(
     name="ohdsi-circe",
-    version=version,
-    description="Python wrapper for the OHDSI R packages",
+    # version=version,
+    description="Python wrapper for the OHDSI Circe R package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/vantage6/python-ohdsi",
@@ -32,9 +30,4 @@ setup(
     package_data={
         "ohdsi.circe.data": ["*.json"],
     },
-    # entry_points={
-    #     'console_scripts': [
-    #         'vserver-local=vantage6.server.cli.server:cli_server'
-    #     ]
-    # }
 )
