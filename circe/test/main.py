8dd13698f7dbd58b628cc14672ce66562ddeeb6c
Frank <mail@franky.codes>
2025-10-24 07:34:08 +0200

    Upgraded package tools to `pyproject.toml`. Fixes tests

--- circe/test/main.py
+++ circe/test/main.py
@@ -7,11 +7,14 @@ import json
 from importlib.resources import files
 
 from ohdsi.circe import (
-    CohortExpression,
-    CohortSqlBuilder,
-    ConceptSetExpression,
-    ConceptSetSqlBuilder,
-    PrintFriendly
+    cohort_expression_from_json,
+    build_cohort_query,
+    concept_set_expression_from_json,
+    build_concept_set_query,
+    cohort_print_friendly,
+    concept_set_print_friendly,
+    concept_set_list_print_friendly,
+    create_generate_options
 )
 
 f_ = files('ohdsi.circe.data')
@@ -22,25 +25,25 @@ concept_set_json = files('ohdsi.circe.data').joinpath('conceptSet.json')\
 concept_set_list_json = files('ohdsi.circe.data')\
     .joinpath('conceptSetList.json').read_text()
 
-cohort = CohortExpression.cohort_expression_from_json(cohort_json)
-options = CohortSqlBuilder.create_generate_options()
-sql = CohortSqlBuilder.build_cohort_query(cohort, options)
+cohort = cohort_expression_from_json(cohort_json)
+options = create_generate_options()
+sql = build_cohort_query(cohort, options)
 print(sql)
 
 concept_json = json.loads(concept_set_json)
 
-concept_set_expression = ConceptSetExpression.concept_set_expression_from_json(
+concept_set_expression = concept_set_expression_from_json(
     concept_json['expression']
 )
 print(concept_set_expression)
 
-concept_set_sql = ConceptSetSqlBuilder.build_concept_set_query(
+concept_set_sql = build_concept_set_query(
     concept_json['expression']
 )
 print(concept_set_sql)
 
 print("***")
-print(PrintFriendly.cohort_print_friendly(cohort))
-print(PrintFriendly.cohort_print_friendly(cohort_json))
-print(PrintFriendly.concept_set_print_friendly(concept_set_json))
-print(PrintFriendly.concept_set_list_print_friendly(concept_set_list_json))
+print(cohort_print_friendly(cohort))
+print(cohort_print_friendly(cohort_json))
+print(concept_set_print_friendly(concept_set_json))
+print(concept_set_list_print_friendly(concept_set_list_json))
